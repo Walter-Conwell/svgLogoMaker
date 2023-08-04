7f983b6ea85713aa5fdccddc4f084d760e8b8c33
Walter-Conwell <131832544+Walter-Conwell@users.noreply.github.com>
2023-08-04 13:49:43 -0600

    fixed repository

---
+++ index.js
@@ -0,0 +1,162 @@
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+
+// const {Square, Circle, Triangle} =  require("./lib/shapes");
+// const SVG = require("./lib/svg");
+// const {writeFile} = require("fs/promises");
+
+// const svgGen = () => {
+
+
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',
+//       name: 'logoText',
+//       message: 'What text should the logo display? Limit of 3...',
+//       validate: (logoText) => logoText.length <= 3 || "No more than 3!"
+//     },
+//     {
+//       type: 'input',
+//       name: 'logoColor',
+//       message: 'What color do you have in mind?',
+//     },
+//     {
+//       type: 'list',
+//       name: 'logoShape',
+//       message: 'How about the shape type?',
+//       choices: ['circle', 'triangle', 'square'],
+//     },
+//     {
+//       type: 'input',
+//       name: 'logoShapeColor',
+//       message: 'And what color for the shape?',
+//     },
+
+// ])
+
+// .then(({logoText, logoColor, logoShape, logoShapeColor}) => {
+//     let shapeEl 
+//     switch (logoShape) {
+//         case "circle": 
+//             shapeEl = new Circle()
+//             break;
+//         case "square": 
+//             shapeEl = new Square()
+//             break;
+//         case "triangle": 
+//             shapeEl = new Triangle()
+//             break;
+
+//     }
+//     shapeEl.setColor(logoShapeColor)
+//     const svg = new SVG()
+//     svg.setText(logoText, logoColor)
+//     svg.setShape(shapeEl)
+//     return writeFile("userLogo.SVG", svg.render())
+// })
+// .then(() => {
+//     console.log("Success!!!")
+// })
+// .catch((err) => {
+//     // const svgChoice = svgGen(userSelection);
+//     //aa
+//     // fs.writeFile("", svgChoice, (err) =>
+//     //   err ? console.log(err) : console.log('Success!')
+//     // );
+//     console.log(err)
+//     console.log("Failure! Try again!")
+//   });
+
+// }
+// //Invokes the function
+// svgGen()
+//variables for the svg to be used later
+
+
+const {Square, Circle, Triangle} =  require("./lib/shapes");
+const SVG = require("./lib/svg");
+const {writeFile} = require("fs/promises");
+
+const svgGen = () => {
+
+
+//.svg logo
+
+// //render() {
+//     return `<circle cx="150" cy="100" r="80" fill="${this.color}" />`;
+//   }
+// const createShape = (color) => {
+// }
+// const createLogo = (data) =>
+//create prompt function for inputs
+//limit of only 3 characters
+//text color
+
+
+//prompts for the svg creation
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'logoText',
+      message: 'Please provide the text, up to three characters.',
+      validate: (logoText) => logoText.length <= 3 || "THREEEEEEEE"
+    },
+    {
+      type: 'input',
+      name: 'logoColor',
+      message: 'What color will the text be?',
+    },
+    {
+      type: 'list',
+      name: 'logoShape',
+      message: 'What will the shape be?',
+      choices: ['circle', 'triangle', 'square'],
+    },
+    {
+      type: 'input',
+      name: 'logoShapeColor',
+      message: 'What color will the shape be?',
+    },
+
+])
+
+
+.then(({logoText, logoColor, logoShape, logoShapeColor}) => {
+    let shapeEl 
+    switch (logoShape) {
+        case "circle": 
+            shapeEl = new Circle()
+            break;
+        case "square": 
+            shapeEl = new Square()
+            break;
+        case "triangle": 
+            shapeEl = new Triangle()
+            //ends function
+            break;
+
+    }
+    shapeEl.setColor(logoShapeColor)
+    const svg = new SVG()
+    svg.setText(logoText, logoColor)
+    svg.setShape(shapeEl)
+    return writeFile("userLogo.SVG", svg.render())
+})
+.then(() => {
+    console.log("Success!!!")
+})
+.catch((err) => {
+    // const svgChoice = svgGen(userSelection);
+    //aa
+    // fs.writeFile("", svgChoice, (err) =>
+    //   err ? console.log(err) : console.log('Success!')
+    // );
+    console.log(err)
+    console.log("Failure! Try again!")
+  });
+
+}
+//Invokes the function
+svgGen()
